7c45ee146f4a094d76950f8dfeed1662be5b4cc1
DRL36 <jnly36@163.com>
2019-03-17 04:41:04 -0700

    'simple'

--- models.py
+++ models.py
@@ -53,14 +53,13 @@ class BiDAF(nn.Module):
         self.enable_selfatt = enable_selfatt
         
         if enable_selfatt:
-            self.selfMatch = layers.StaticDotAttention(memory_size = 2 * self.d, 
-                            input_size = 2 * self.d, attention_size = 2 * self.d,
-                            drop_prob=drop_prob)
+            self.selfMatch = layers.BiDAFAttention(hidden_size=2 * self.d,
+                                         drop_prob=drop_prob)
                                          
-        # self.mod = layers.RNNEncoder(input_size=2 * self.d,
-        #                                  hidden_size=self.d,
-        #                                  num_layers=2,
-        #                                  drop_prob=drop_prob)
+        self.mod = layers.RNNEncoder(input_size=4 * self.d,
+                                          hidden_size=2 *self.d,
+                                          num_layers=2,
+                                          drop_prob=drop_prob)
 
         self.out = layers.BiDAFOutput(hidden_size=2* self.d,
                                       drop_prob=drop_prob)
@@ -99,11 +98,11 @@ class BiDAF(nn.Module):
         assert att.size(2) == 2 * self.d
 
         if self.enable_selfatt:
-            self_match = self.selfMatch(c_enc, c_enc, c_mask)
+            self_match = self.att(c_enc, c_enc, c_mask, c_mask)
             assert att.size(2) == 2 * self.d
 
-            #mod = self.mod(torch.cat((self_match, att), dim=2), c_len)        # (batch_size, c_len, 2 * d)
-            out = self.out(torch.cat((self_match, att), dim=2), c_mask)
+            mod = self.mod(torch.cat((self_match, att), dim=2), c_len)        # (batch_size, c_len, 2 * d)
+            out = self.out(torch.cat((self_match, att), dim=2), mod, c_mask)
         else:
             mod = self.mod(att, c_len)        # (batch_size, c_len, 2 * d)
             #assert mod.size(2) == 2 * self.d
